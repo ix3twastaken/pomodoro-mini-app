c1de508e62bcded6daf8469093e87a71f73f5cb4
ix3twastaken <harkoiiika38@gmail.com>
2026-02-08 21:55:04 +0300

    Changed the notification library

--- notifications.py
+++ notifications.py
@@ -1,24 +1,22 @@
 import os
-from plyer import notification
-from config import PRESETS
+from notifypy import Notify
+from config import PRESETS, icon_path
 import dearpygui.dearpygui as dpg
 
 def timer_notify(user_data: str):
     preset = dpg.get_value("presets")
     work_duration = PRESETS[preset][0] // 60
     chill_duration = PRESETS[preset][1] // 60
-    
+    notification = Notify()
     if user_data == "start":
-        notification.notify(
-            title='Pomodoro',
-            app_icon=os.path.join(os.path.dirname(__file__), "tomato.ico"), #This icon was designed using resources from Flaticon.com.
-            message=f'Время работать! {work_duration} минут до перерыва',
-            app_name='Pomodoro Timer'
-        )
+        notification.title='Pomodoro'
+        notification.icon=os.path.join(os.path.dirname(__file__), "icon.ico") #This icon was designed using resources from Flaticon.com.
+        notification.message=f'Время работать! {work_duration} минут до перерыва'
+        notification.application_name='Pomodoro'
+        notification.send(block=False)
     else:
-        notification.notify(
-            title='Pomodoro',
-            app_icon=os.path.join(os.path.dirname(__file__), "tomato.ico"), #This icon was designed using resources from Flaticon.com.
-            message=f'Время отдыхать! {chill_duration} минут перерыва',
-            app_name='Pomodoro Timer'
-        )
+        notification.title='Pomodoro'
+        notification.icon=os.path.join(os.path.dirname(__file__), "icon.ico") #This icon was designed using resources from Flaticon.com.
+        notification.message=f'Время отдыхать! {chill_duration} минут перерыва'
+        notification.application_name='Pomodoro'
+        notification.send(block=False)

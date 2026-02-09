59aee47bde5e95db4e4653cb441c12f3233671c4
ix3twastaken <harkoiiika38@gmail.com>
2026-02-09 19:23:29 +0300

    Refactor: add replace_button() for readability

--- gui.py
+++ gui.py
@@ -20,29 +20,57 @@ def setup_gui():
         with dpg.group():
             dpg.add_button(
                 label="Start",
-                tag="start",
+                tag="start_btn",
                 callback=handle_start,
                 width=100,
                 height=50
             )
             dpg.add_button(
                 label="Stop",
-                tag="stop",
+                tag="stop_btn",
                 callback=handle_stop,
                 width=100,
                 height=50,
                 show=False
             )
+            dpg.add_button(
+                label="Pause",
+                tag="pause_btn",
+                callback=handle_pause,
+                width=100,
+                height=50
+            )
+            dpg.add_button(
+                label="Resume",
+                tag="resume_btn",
+                callback=handle_resume,
+                width=100,
+                height=50,
+                show=False
+            )
+            
+def replace_button(
+        btn_to_replace_tag: str,
+        btn_to_insert_tag: str      
+):
+    dpg.hide_item(btn_to_replace_tag)
+    dpg.show_item(btn_to_insert_tag)
 
 def handle_start():
-    dpg.hide_item("start")
-    dpg.show_item("stop")
+    replace_button("start_btn", "stop_btn")
     dpg.configure_item("presets", enabled=False)
     timer.start_timer()
 
 def handle_stop():
-    dpg.hide_item("stop")
-    dpg.show_item("start")
+    replace_button("stop_btn", "start_btn")
     timer.stop_timer()
     dpg.configure_item("presets", enabled=True)
     dpg.set_value("time", "00:00")
+    
+def handle_pause():
+    replace_button("pause_btn", "resume_btn")
+    
+def handle_resume():
+    replace_button("resume_btn", "pause_btn")
+
+    

1a7b153b234283527b23ec7ca5774d3233f99523
ix3twastaken <harkoiiika38@gmail.com>
2026-02-20 19:49:59 +0300

    Add: align UI elements

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ import dearpygui.dearpygui as dpg
 
 import timer
 from themes import create_theme_imgui_dark, create_theme_imgui_light
-from config import WORK_TIME, REST_TIME
+from config import *
 from utils.sys_utils import get_file_path
 
 font_path = get_file_path("Eitai.otf", __file__)
@@ -24,49 +24,72 @@ def setup_default_theme():
     
 def setup_gui():
     with dpg.window(tag="Window"):
-        with dpg.menu_bar():
+        with dpg.menu_bar(tag="menu_bar"):
             with dpg.menu(label="Themes", tag="themes_menu"):
                 dpg.add_menu_item(label="Dark", callback=theme_callback, user_data="dark")
                 dpg.add_menu_item(label="Light", callback=theme_callback, user_data="light")
         
-        with dpg.group():
-            dpg.add_text(default_value="Work time")
-            dpg.add_listbox(
-                tag="work_time_presets",
-                items=list(WORK_TIME.keys()),
-                default_value="25",
-                width=45
-            )
-            dpg.add_text(default_value="Rest time")
-            dpg.add_listbox(
-                tag="rest_time_presets",
-                items=list(REST_TIME.keys()),
-                default_value="5",
-                width=45
-            )
-
-        with dpg.group():
-            dpg.add_text(default_value="Time")
+        with dpg.group(tag="time_block", horizontal=False):
+            dpg.add_separator()
+            dpg.add_spacer(height=10)   
+            dpg.add_text(default_value="Time", 
+                         tag="time_label",
+                         indent=132
+                         )
             dpg.add_text(default_value="00:00", 
-                         tag="time"
-                        )       
+                         tag="time",
+                         indent=128
+                         )
+            dpg.add_spacer(height=10)   
+            dpg.add_separator()
+            dpg.add_spacer(height=10)
+        
+        with dpg.table(tag="presets_table", header_row=False, borders_outerH=False, borders_outerV=False, borders_innerH=False, borders_innerV=False):
+            dpg.add_table_column(tag="col_1")
+            dpg.add_table_column(tag="col_2")
 
-        with dpg.group():
+            with dpg.table_row(tag="row_1"):
+                with dpg.group(tag="work_time_listbox"):
+                    dpg.add_text(default_value="Work time",
+                                 indent=24)
+                    dpg.add_listbox(
+                        tag="work_time_presets",
+                        items=list(WORK_TIME.keys()),
+                        default_value="25",
+                        width=152
+                    )
+                with dpg.group(tag="rest_time_listbox"):
+                    dpg.add_text(default_value="Rest time",
+                                 indent=24)
+                    dpg.add_listbox(
+                    tag="rest_time_presets",
+                    items=list(REST_TIME.keys()),
+                    default_value="5",
+                    width=152,
+                    indent=4
+                    )
+                    
+        dpg.add_spacer(height=10)   
+        dpg.add_separator()
+        dpg.add_spacer(height=10)
+        
+        with dpg.group(tag="start_stop_btns", horizontal=True):
             dpg.add_button(
                 label="Start",
                 tag="start_btn",
                 callback=handle_start,
-                width=100,
+                width=318,
                 height=50
             )
             dpg.add_button(
                 label="Stop",
                 tag="stop_btn",
                 callback=handle_stop,
-                width=100,
+                width=318,
                 height=50,
                 show=False
-            )     
+            )
+ 
     setup_default_font() 
     setup_default_theme()
             
